a6594c7ad0d0ad5dbd103529507f74d654b3a3d7
DanaJoffe <djoffe94@gmail.com>
2018-05-06 23:49:05 +0300

    GUI-Settings

---
+++ ImageService/GUI/Models/SettingsModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.Models
+{
+    public class SettingsModel
+    {
+        #region Notify Changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+
+        public SettingsModel()
+        {
+            this.HandlersList = GetHandlers();
+            SetConfigInfo();
+        }
+
+        private void SetConfigInfo()
+        {
+            this.OutputDirectory = "path to some dir";
+            this.SourceName = "some source name";
+            this.LogName = "loglogloglog";
+            this.ThumbnailSize = "300000";
+        }
+
+        private string m_thumbnailSize;
+        public string ThumbnailSize
+        {
+            get { return m_thumbnailSize; }
+            set
+            {
+                m_thumbnailSize = value;
+                NotifyPropertyChanged("ThumbnailSize");
+            }
+        }
+
+        private string m_logName;
+        public string LogName
+        {
+            get { return m_logName; }
+            set
+            {
+                m_logName = value;
+                NotifyPropertyChanged("LogName");
+            }
+        }
+
+
+        private string m_sourceName;
+        public string SourceName
+        {
+            get { return m_sourceName; }
+            set
+            {
+                m_sourceName = value;
+                NotifyPropertyChanged("SourceName");
+            }
+        }
+
+        private string m_outputDirectory;
+        public string OutputDirectory
+        {
+            get { return m_outputDirectory; }
+            set
+            {
+                m_outputDirectory = value;
+                NotifyPropertyChanged("OutputDirectory");
+            }
+        }
+
+        public ObservableCollection<string> HandlersList { get; private set; }
+
+        private string m_chosenHandler;
+        public string ChosenHandler
+        {
+            get { return m_chosenHandler; }
+            set
+            {
+                m_chosenHandler = value;
+                NotifyPropertyChanged("ChosenHandler");
+
+            }
+        }
+
+        // HandlersList management
+        private ObservableCollection<string> GetHandlers()
+        {
+            ObservableCollection<string> handlersList = new ObservableCollection<string>() { "give me 'HAND'!", "HAND!", "give me 'LER'!", "LER!", "What came out??", "HANDLER!!"};
+            return handlersList;
+        }
+
+        public bool RemoveHandler(string handler)
+        {
+            bool result = true;
+            if (result)
+            {
+                this.HandlersList.Remove(handler);
+            }
+            return result;
+        }
+    }
+}
---
+++ ImageService/GUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+//using System.Diagnostics;
+//using System.Text;
+//using System.Windows.Input;
+//using BasicMVVMQuickstart_Desktop.Model;
+//using System.ComponentModel;
+//using Microsoft.Practices.Prism.Commands;
+
+
+namespace GUI.ViewModels
+{
+    class MainWindowViewModel
+    {
+        public SettingsViewModel SettingsViewModel { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Debug.WriteLine(" ");
+            Debug.WriteLine(" ");
+            Debug.WriteLine(" ");
+            Debug.WriteLine(" ");
+
+            Debug.WriteLine("INIT");
+
+            Debug.WriteLine(" ");
+            Debug.WriteLine(" ");
+            Debug.WriteLine(" ");
+            Debug.WriteLine(" ");
+            if (ConnectToServer())
+            {
+                this.SettingsViewModel = new SettingsViewModel();
+            } else
+            {
+                this.BackgroundColor = "gray";
+                //TODO: change color
+                //TODO: Style to "Settings" head (and Settings head itself)
+            }
+        }
+
+        private string m_backgroundColor;
+        public string BackgroundColor
+        {
+            get { return m_backgroundColor; }
+            set
+            {
+                m_backgroundColor = value;
+            }
+        }
+
+        private bool ConnectToServer()
+        {
+            bool result = true;
+            return result;
+        }
+
+        //private void PropertyChanged(object sender, PropertyChangedEventArgs e)
+        //{
+        //    //var command = this.SubmitCommand as DelegateCommand<object>;
+        //    //command.RaiseCanExecuteChanged();
+        //}
+
+        //public ICommand SubmitCommand { get; private set; }
+
+        //public ICommand ResetCommand { get; private set; }
+
+
+
+        //private void OnSubmit(object obj)
+        //{
+        //    Debug.WriteLine(this.BuildResultString());
+        //}
+
+        //private bool CanSubmit(object obj)
+        //{
+        //    if (string.IsNullOrEmpty(this.QuestionnaireViewModel.Questionnaire.Name))
+        //    {
+        //        return false;
+        //    }
+        //    if (this.QuestionnaireViewModel.Questionnaire.Age < 0 || this.QuestionnaireViewModel.Questionnaire.Age > 120)
+        //    {
+        //        return false;
+        //    }
+        //    if (string.IsNullOrEmpty(this.QuestionnaireViewModel.Questionnaire.Quest))
+        //    {
+        //        return false;
+        //    }
+        //    if (string.IsNullOrEmpty(this.QuestionnaireViewModel.Questionnaire.FavoriteColor))
+        //    {
+        //        return false;
+        //    }
+        //    return true;
+        //}
+
+
+        //private void OnReset()
+        //{
+        //    this.QuestionnaireViewModel.Questionnaire = new Questionnaire();
+        //}
+
+        //private string BuildResultString()
+        //{
+        //    StringBuilder builder = new StringBuilder();
+        //    builder.Append("Name: ");
+        //    builder.Append(this.QuestionnaireViewModel.Questionnaire.Name);
+        //    builder.Append("\nAge: ");
+        //    builder.Append(this.QuestionnaireViewModel.Questionnaire.Age);
+        //    builder.Append("\nQuestion 1: ");
+        //    builder.Append(this.QuestionnaireViewModel.Questionnaire.Quest);
+        //    builder.Append("\nQuestion 2: ");
+        //    builder.Append(this.QuestionnaireViewModel.Questionnaire.FavoriteColor);
+        //    return builder.ToString();
+        //}
+
+    }
+}
