4803a40150e8bad4cbbab3d213be632167ed42f5
unknown <maayanrhv@gmail.com>
2018-05-17 21:49:01 +0300

    fixed bug integrating logCommand

--- ImageService/ImageService/Server/ClientHandler.cs
+++ ImageService/ImageService/Server/ClientHandler.cs
@@ -48,7 +48,7 @@ namespace ImageService.Server
                         (int)CommandEnum.GetLogCommand, null, out res2);
                     if (res1 && res2)
                     {
-                        //writer.Write(commandRes1);
+                        writer.Write(commandRes1);
                         writer.Write(commandRes2);
                     }
                 } catch (Exception e)
