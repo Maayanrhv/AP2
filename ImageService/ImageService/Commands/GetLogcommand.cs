0c5d44fd81f76bf385acc50dfc72e0103e46a04f
unknown <maayanrhv@gmail.com>
2018-05-17 12:42:05 +0300

    updated getLog & getConfig Commands

--- ImageService/ImageService/Commands/GetLogcommand.cs
+++ ImageService/ImageService/Commands/GetLogcommand.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using ImageService.Communication;
+using ImageService.Infrastructure.Enums;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ImageService.Commands
 {
-    class GetLogcommand
+    public class GetLogcommand : ICommand
     {
+        public string Execute(string[] args, out bool result)
+        {
+            bool stopGetLogs = false;
+            string[] logsArray;
+            List<string> logsList = new List<string>();
+            string logName = ConfigurationManager.AppSettings["LogName"];
+            EventLog log = new EventLog(logName, "."); // "." is the local computer
+            EventLogEntryCollection entries = log.Entries;
+
+            foreach (EventLogEntry entry in entries)
+            {
+                if (stopGetLogs)
+                    break;
+                string msg = entry.Message;
+                if (msg.Equals("In OnStart"))
+                    stopGetLogs = true;
+                logsList.Add(msg);
+            }
+
+            string convertEachString = JsonConvert.SerializeObject(logsList);
+            if (convertEachString == null || !logsList.Any())
+            {
+                result = false;
+                return null;
+            }
+            // if list is not empty
+                logsArray = logsList.ToArray();
+                CommunicationProtocol commandSendArgs = new CommunicationProtocol(
+                    (int)CommandEnum.GetConfigCommand, logsArray);
+                result = true;
+                return JsonConvert.SerializeObject(commandSendArgs);
+        }
     }
 }
