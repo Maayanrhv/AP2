d0c74f88a653d34318165f976900a84eb71cbcad
unknown <maayanrhv@gmail.com>
2018-06-04 13:14:22 +0300

    added web project + home page

---
+++ ImageService/ImageServiceWeb/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageServiceWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Config()
+        {
+            ViewBag.Message = "Your application Configuration page.";
+
+            return View();
+        }
+
+        public ActionResult Photos()
+        {
+            ViewBag.Message = "Your photos page.";
+
+            return View();
+        }
+
+        public ActionResult Logs()
+        {
+            ViewBag.Message = "Your Logs page.";
+
+            return View();
+        }
+    }
+}
