14bfdb4a2b1b9a03715d4c45c3d6105813b59514
DanaJoffe <djoffe94@gmail.com>
2018-06-04 15:29:07 +0300

    initial structur of web pages

---
+++ ImageService/ImageServiceWeb/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageServiceWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public ActionResult ImageWeb()
+        {
+            return View();
+        }
+        public ActionResult Config()
+        {
+            return View();
+        }
+        public ActionResult Logs()
+        {
+            return View();
+        }
+        public ActionResult Photos()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
